78d096a57d9d78ee11238e5e1856dc910279c4f2
Pawel Lorenc <pawellorencc@gmail.com>
2022-08-08 18:23:27 +0200

    seeder with fakedata implemented

---
+++ Data/Context/Seeder/MaterialSeeder.cs
@@ -0,0 +1,53 @@
+﻿namespace Data.Context.Seeder
+{
+    public class MaterialSeeder
+    {
+        private readonly EducationalMaterialContext _context;
+
+        public MaterialSeeder(EducationalMaterialContext context)
+        {
+            _context = context;
+        }
+        public void Seed()
+        {
+            if (_context.Database.CanConnect())
+            {
+                if (!_context.MaterialNavigationPoints.Any())
+                {
+                    var materials = GetMaterials;
+                    _context.MaterialNavigationPoints.AddRange(materials);
+                }
+            }
+        }
+        private IEnumerable<MaterialNavigationPoint> GetMaterials()
+        {
+            var materials = new List<MaterialNavigationPoint>();
+
+            var material = new MaterialNavigationPoint()
+            {
+                Title = "Object Orientated programming",
+                Description = "Introduction to object oriantated programming",
+                Location = "CD",
+            };
+
+            var author = new Author()
+            {
+                Name = "Pawel Lorenc",
+                Description = "Tall, blue eyed, handsome",
+                MaterialCounter = 1
+            };
+
+            var MaterialType = new MaterialType()
+            {
+                Name = "Video Tutorial",
+                Definition = "video tutorial 
+is a video material that
+focuses mostly on guiding
+step - by - step in dedicated
+topic",
+
+            }
+        }
+
+    }
+}
