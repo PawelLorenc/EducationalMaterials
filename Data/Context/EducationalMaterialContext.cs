316161248b2cfb07e9a270a09bb09b2c04170433
Pawel Lorenc <pawellorencc@gmail.com>
2022-08-08 14:24:41 +0200

    fluent api implemented

--- Data/Context/EducationalMaterialContext.cs
+++ Data/Context/EducationalMaterialContext.cs
@@ -1,12 +1,77 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Data.Context
+﻿namespace Data.Context
 {
-    public class EducationalMaterialContext
+    public class EducationalMaterialContext : DbContext
     {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<MaterialNavigationPoint> MaterialNavigationPoints { get; set; }
+        public DbSet<MaterialType> MaterialTypes { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        public EducationalMaterialContext(DbContextOptions<EducationalMaterialContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<MaterialNavigationPoint>(m =>
+            {
+                m.HasOne(m => m.Author)
+                .WithMany(a => a.Materials)
+                .HasForeignKey(m => m.AuthorId);
+                m.HasOne(m => m.Material)
+                .WithOne(mt => mt.MaterianNavigationPoint)
+                .HasForeignKey<MaterialNavigationPoint>(m => m.MaterialTypeId);
+                m.HasMany(m => m.Reviews)
+                .WithOne(r => r.Material)
+                .HasForeignKey(r => r.MaterialNAvigationPointId);
+                m.Property(x => x.Title)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+                m.Property(x => x.Description)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+                m.Property(x => x.Location)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+            });
+            builder.Entity<Author>(a =>
+            {
+                a.HasMany(a => a.Materials)
+                .WithOne(m => m.Author)
+                .HasForeignKey(m => m.MaterialTypeId);
+                a.Property(x => x.Name)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+                a.Property(x => x.Description)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+            });
+            builder.Entity<MaterialType>(m =>
+            {
+                m.HasOne(m => m.MaterianNavigationPoint)
+                .WithOne(x => x.Material)
+                .HasForeignKey<MaterialType>(m => m.MaterialNavigationPointId);
+                m.Property(m => m.Name)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+                m.Property(x => x.Definition)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+            });
+            builder.Entity<Review>(m =>
+            {
+                m.HasOne(a => a.Material)
+                .WithMany(x => x.Reviews)
+                .HasForeignKey(a => a.MaterialNAvigationPointId);
+                m.Property(x => x.TextReview)
+                .HasColumnType("varchar(200)")
+                .IsRequired();
+                m.Property(x => x.PointReview)
+                .IsRequired();
+            });
+
+        }
+
     }
 }
--- Data/Usings.cs
+++ Data/Usings.cs
@@ -1,1 +1,8 @@
 ﻿global using System.ComponentModel.DataAnnotations;
+global using Microsoft.EntityFrameworkCore;
+global using Data.Entities;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
